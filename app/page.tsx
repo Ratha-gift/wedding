738a094db8aaaafd80c604b9c3043272d130e7b0
HANG <hloveh417@gmail.com>
2026-01-01 13:06:11 +0700

    Done with homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
-import LoginPage from "..//login/login";
+import LoginPage from "../login/login";   // ← probably ok
+// import Page from "../Homepage/page";     // ← this name is dangerous
+
+// export function Page() {                           // ←←← PROBLEM HERE
+//   return <HomePage />;
+// }
+
 export default function HomePage() {
   return <LoginPage />;
-} 
+}
